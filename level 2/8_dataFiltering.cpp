7e3af133820167b61543dac03f5d4415fa343146
LEE SEUNG HO <117427127+SeunghoL22@users.noreply.github.com>
2025-11-21 08:37:59 +0900

    확장자 추가

---
+++ level 2/8_dataFiltering.cpp
@@ -0,0 +1,13 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/250121
+// [PCCE 기출문제] 10번 / 데이터 분석
+
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> data, string ext, int val_ext, string sort_by) {
+    vector<vector<int>> answer;
+    return answer;
+}
